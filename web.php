28b157118cf9e0dc9104e99b1f55d159fb9b4c7a
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-08-29 21:30:14 +0600

    Update web.php

--- web.php
+++ web.php
@@ -1,1 +1,10 @@
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/clear', function(){
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('optimize:clear');
+    return dd('Done');
+});
