e1a76fcacb28e4bc1ef1dd9c179f9550cdedd575
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-02-14 10:26:06 +0600

    Update web.php

--- web.php
+++ web.php
@@ -8,3 +8,17 @@ Route::get('/clear', function(){
     Artisan::call('optimize:clear');
     return dd('Done');
 });
+
+
+// Clear application cache:
+use Illuminate\Support\Facades\Artisan;
+Route::get('/clear', function() {
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('queue:clear');
+    Artisan::call('event:clear');
+    Artisan::call('optimize:clear');
+    Artisan::call('cache:clear');
+    return __('Application cache has been cleared ✔️');
+});
